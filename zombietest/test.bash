f197aa8f6a020649dc8091a2ffda1b5ce1f94aae
Peter Bourgon <peter@bourgon.org>
2015-09-28 22:46:18 +0200

    Try Circle

--- zombietest/test.bash
+++ zombietest/test.bash
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-RC=0
+function zombies() {
+	if [ -z "$CIRCLECI" ]
+	then
+		docker exec $C ps -o pid,stat | grep Z | wc -l
+	else
+		# https://circleci.com/docs/docker#docker-exec
+		sudo lxc-attach -n "$(docker inspect --format '{{.Id}}' $C)" -- sh -c "ps -o pid,stat | grep Z | wc -l"
+	fi
+}
+
+function stop_rm() {
+	docker stop $1
+	docker rm $1
+}
+
 SLEEP=1
+RC=0
 
 C=$(docker run -d zombietest /runsvinit -reap=false)
 sleep $SLEEP
-NOREAP=$(docker exec $C ps -o pid,stat | grep Z | wc -l)
+NOREAP=$(zombies)
 echo -n without reaping, we have $NOREAP zombies...
 if [ "$NOREAP" -le "0" ]
 then
@@ -14,12 +29,11 @@ then
 else
 	echo " good"
 fi
-docker stop $C >/dev/null
-docker rm $C >/dev/null
+stop_rm $C
 
 C=$(docker run -d zombietest /runsvinit)
 sleep $SLEEP
-YESREAP=$(docker exec $C ps -o pid,stat | grep Z | wc -l)
+YESREAP=$(zombies)
 echo -n with reaping, we have $YESREAP zombies...
 if [ "$YESREAP" -gt "0" ]
 then
@@ -28,7 +42,6 @@ then
 else
 	echo " good"
 fi
-docker stop $C >/dev/null
-docker rm $C >/dev/null
+stop_rm $C
 
 exit $RC
